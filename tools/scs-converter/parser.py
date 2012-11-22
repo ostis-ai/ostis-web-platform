4d146736f38547c464fd55e12bac3d523e127289
Denis Koronchik <denis.koronchik@gmail.com>
2012-11-23 01:06:32 +0300

    added license

--- tools/scs-converter/parser.py
+++ tools/scs-converter/parser.py
@@ -1,3 +1,25 @@
+"""
+-----------------------------------------------------------------------------
+This source file is part of OSTIS (Open Semantic Technology for Intelligent Systems)
+For the latest info, see http://www.ostis.net
+
+Copyright (c) 2012 OSTIS
+
+OSTIS is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OSTIS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with OSTIS. If not, see <http://www.gnu.org/licenses/>.
+-----------------------------------------------------------------------------
+"""
+
 import re, sys, traceback
 from pyparsing import Word, Literal, Forward, Regex, Group, ZeroOrMore, SkipTo, ParserElement
 from pyparsing import OneOrMore, srange, Keyword, QuotedString, ParseResults, Optional, cStyleComment
