7cfbc5eacd9bcccb82513a77784e6fd4d686ca10
ShunkevichDV <shu.dv@tut.by>
2015-10-25 19:06:54 +0300

    IMS KB copying script changed

--- scripts/copy_ims_kb.sh
+++ scripts/copy_ims_kb.sh
@@ -8,13 +8,13 @@ fi
 
 cd ../
 
-cp -a ims.ostis.kb/knowledge_base_IMS/doc_technology_ostis/section_unificated_semantic_network_and_representation/ ims.ostis.kb_copy/
-cp -a ims.ostis.kb/knowledge_base_IMS/doc_technology_ostis/section_unificated_models/ ims.ostis.kb_copy/
-cp -a ims.ostis.kb/knowledge_base_IMS/doc_technology_ostis/section_basic_model_of_the_unified_processing_of_semantic_networks/ ims.ostis.kb_copy/
-cp -a ims.ostis.kb/knowledge_base_IMS/doc_technology_ostis/section_library_OSTIS/section_library_of_reusable_components_interfaces/lib_ui_menu/ ims.ostis.kb_copy/
-cp -a ims.ostis.kb/knowledge_base_IMS/doc_technology_ostis/section_library_OSTIS/section_library_of_reusable_components_processing_machinery_knowledge/lib_c_agents/ ims.ostis.kb_copy/
-cp -a ims.ostis.kb/knowledge_base_IMS/doc_technology_ostis/section_library_OSTIS/section_library_of_reusable_components_processing_machinery_knowledge/lib_scp_agents/ ims.ostis.kb_copy/
-cp -a ims.ostis.kb/knowledge_base_IMS/doc_technology_ostis/section_library_OSTIS/section_library_of_reusable_components_processing_machinery_knowledge/section_library_of_reusable_programs_for_sc_text_processing/lib_scp_program/ ims.ostis.kb_copy/
+cp -a ims.ostis.kb/ims/doc_technology_ostis/semantic_network_represent/ ims.ostis.kb_copy/
+cp -a ims.ostis.kb/ims/doc_technology_ostis/unificated_models/ ims.ostis.kb_copy/
+cp -a ims.ostis.kb/ims/doc_technology_ostis/semantic_networks_processing/ ims.ostis.kb_copy/
+cp -a ims.ostis.kb/ims/doc_technology_ostis/library_OSTIS/components_interface/ui_menu/ ims.ostis.kb_copy/
+cp -a ims.ostis.kb/ims/doc_technology_ostis/library_OSTIS/components_kpm/lib_c_agents/ ims.ostis.kb_copy/
+cp -a ims.ostis.kb/ims/doc_technology_ostis/library_OSTIS/components_kpm/lib_scp_agents/ ims.ostis.kb_copy/
+cp -a ims.ostis.kb/ims/doc_technology_ostis/library_OSTIS/components_kpm/programs_for_sc_text_processing/scp_program/ ims.ostis.kb_copy/
 cp -a ims.ostis.kb/to_check/ ims.ostis.kb_copy/
 cp -a ims.ostis.kb/ui/ ims.ostis.kb_copy/
 rm -rf ims.ostis.kb_copy/ui/menu
