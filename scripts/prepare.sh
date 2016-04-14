3a58a37e8b7db901259775e3b2c49859ee7d2149
Kirill Rusetski <rusetski.k@gmail.com>
2016-04-14 19:40:03 +0000

    adjusted sc-web source url

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -32,7 +32,7 @@ clone_project()
 stage "Clone projects"
 
 clone_project https://github.com/ShunkevichDV/sc-machine.git sc-machine master
-clone_project https://github.com/deniskoronchik/sc-web.git sc-web master
+clone_project https://github.com/Ivan-Zhukau/sc-web.git sc-web master
 clone_project https://github.com/ShunkevichDV/ims.ostis.kb.git ims.ostis.kb master
 
 stage "Prepare projects"
