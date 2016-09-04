2544001f3f1bd30dc945fc4601d020b105674522
shunkevichdv <shunkevichdv@gmail.com>
2016-09-04 20:23:35 +0300

    switched to master kb version

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -33,7 +33,7 @@ stage "Clone projects"
 
 clone_project https://github.com/deniskoronchik/sc-machine.git sc-machine master
 clone_project https://github.com/deniskoronchik/sc-web.git sc-web master
-clone_project https://github.com/ShunkevichDV/ims.ostis.kb.git ims.ostis.kb dev
+clone_project https://github.com/ShunkevichDV/ims.ostis.kb.git ims.ostis.kb master
 
 stage "Prepare projects"
 
