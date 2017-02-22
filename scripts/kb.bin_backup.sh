d61620e9adeadcf4f84a6cb6ac03dae6d04d71c9
shunkevichdv <shunkevichdv@gmail.com>
2017-02-22 20:35:22 +0300

    kb.bin git backup script added

---
+++ scripts/kb.bin_backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=../sc-machine/bin
+
+if [ ! -d "../kb.bin_backup" ]; then
+	mkdir ../kb.bin_backup
+	cd ../kb.bin_backup
+	git init
+	cd -
+fi
+
+cd ../
+rm -rf kb.bin_backup/kb.bin
+cp -a kb.bin kb.bin_backup/
+
+cd ./kb.bin_backup
+git add --all && git commit -m "$(date)"
+
+cd -
+
+
