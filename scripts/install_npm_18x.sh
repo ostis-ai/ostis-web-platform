6f2c4f485034a643aeaba80f668133e5787af8bf
Daniil Shunkevich <shunkevichdv@gmail.com>
2018-12-15 09:04:43 +0300

    Install npm for ubuntu 18x versions added

---
+++ scripts/install_npm_18x.sh
@@ -0,0 +1,13 @@
+sudo apt remove --purge nodejs npm
+sudo apt clean
+sudo apt autoclean
+sudo apt install -f
+sudo apt autoremove
+
+sudo apt install curl
+sudo curl -sL https://deb.nodesource.com/setup_10.x
+sudo apt-get install -y nodejs
+sudo curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg > pubkey.gpg
+sudo apt-key add pubkey.gpg
+sudo apt-get update && sudo apt-get install yarn
+rm pubkey.gpg
