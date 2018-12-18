7c8c2a4dbac29d5bda1b26cba53f77c724be9d23
Daniil Shunkevich <shunkevichdv@gmail.com>
2018-12-18 15:46:45 +0300

    Npm script updated

--- scripts/install_npm_18x.sh
+++ scripts/install_npm_18x.sh
@@ -5,7 +5,7 @@ sudo apt install -f
 sudo apt autoremove
 
 sudo apt install curl
-sudo curl -sL https://deb.nodesource.com/setup_10.x
+sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg > pubkey.gpg
 sudo apt-key add pubkey.gpg
