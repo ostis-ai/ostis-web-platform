c2bc5fd92550f6d904554fc2c7c379fa9476b3d3
MikhailSadovsky <mishachess@mail.ru>
2020-04-03 15:59:09 +0300

    feat(build_kb): Adding support of multiple source folders

--- scripts/build_kb.sh
+++ scripts/build_kb.sh
@@ -6,6 +6,6 @@ if [ ! -d "../kb.bin" ]; then
 fi
 
 cd ..
-sc-machine/bin/sc-builder -f -c -i ims.ostis.kb -o kb.bin -s config/sc-web.ini -e sc-machine/bin/extensions
+sc-machine/bin/sc-builder -f -c -i repo.path -o kb.bin -s config/sc-web.ini -e sc-machine/bin/extensions
 
 
