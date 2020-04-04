3bc79d456c27557e5e8575411e8254611023c37f
MikhailSadovsky <mishachess@mail.ru>
2020-04-04 13:00:32 +0300

    build(prepare): Update repository links

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -31,9 +31,9 @@ clone_project()
 
 stage "Clone projects"
 
-clone_project https://github.com/MikhailSadovsky/sc-machine.git sc-machine release/0.6.0
+clone_project https://github.com/ShunkevichDV/sc-machine.git sc-machine 0.6.0
 clone_project https://github.com/MikhailSadovsky/sc-web.git sc-web 0.6.0
-clone_project https://github.com/MikhailSadovsky/ims.ostis.kb.git ims.ostis.kb 0.6.0
+clone_project https://github.com/ShunkevichDV/ims.ostis.kb.git ims.ostis.kb 0.6.0
 
 stage "Prepare projects"
 
