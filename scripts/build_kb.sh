cbf384c1f05c4f8930f684c32cc37ed868dc9473
NikitaZotov <nikita.zotov.belarus@gmail.com>
2021-10-19 18:41:59 +0300

    feat(build): improve kb paths usability

--- scripts/build_kb.sh
+++ scripts/build_kb.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+REPO_PATH_FILE="repo.path"
+PREPARED_KB="prepared_kb"
 
 export LD_LIBRARY_PATH=./sc-machine/bin
 if [ ! -d "../kb.bin" ]; then
     mkdir ../kb.bin
 fi
 
-
-python3 kb_scripts/prepare_kb.py
+python3 kb_scripts/prepare_kb.py "${PWD%/[^/]*}" $PREPARED_KB $REPO_PATH_FILE
 
 cd ..
-sc-machine/bin/sc-builder -f -c -i prepared_kb/repo.path -o kb.bin -s config/sc-web.ini -e sc-machine/bin/extensions
+sc-machine/bin/sc-builder -f -c -i $PREPARED_KB/$REPO_PATH_FILE -o kb.bin -s config/sc-web.ini -e sc-machine/bin/extensions
 
 
