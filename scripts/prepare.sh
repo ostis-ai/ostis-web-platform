d78d9dd21b5e2ebfc855d45c45c99493053bfadd
FallenChromium <fallenchromium@gmail.com>
2022-10-12 03:48:02 +0300

    feat: move ims.ostis.kb to submodules

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -44,9 +44,8 @@ clone_project()
 
 stage "Clone projects"
 
-clone_project https://github.com/ostis-ai/sc-machine.git sc-machine 0.6.1
-clone_project https://github.com/ostis-ai/sc-web.git sc-web 0.6.1
-clone_project https://github.com/ostis-ai/ims.ostis.kb.git ims.ostis.kb 0.2.1
+clone_project https://github.com/ostis-ai/sc-machine.git sc-machine main
+clone_project https://github.com/ostis-ai/sc-web.git sc-web main
 
 git submodule update --init --recursive
 
