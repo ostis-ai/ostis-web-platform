dd9d3926c0acb3b25f1f3b84942f8170059e6081
NikiforovSergei <nikiforov.sergei.al@gmail.com>
2023-02-06 20:19:26 +0300

    scripts: move to new template search version

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -44,7 +44,7 @@ clone_project()
 
 stage "Clone projects"
 
-clone_project https://github.com/NikitaZotov/sc-machine-1.git sc-machine 87902ac7a6ae8088d54a4d5bce73592be2179815
+clone_project https://github.com/NikitaZotov/sc-machine-1.git sc-machine 2f187adcee2d76eb368a91b0dad149d85eabdd65
 clone_project https://github.com/ostis-ai/sc-web.git sc-web d0ab25902d5accf94201548717c479842d4418f7
 
 git submodule update --init --recursive
