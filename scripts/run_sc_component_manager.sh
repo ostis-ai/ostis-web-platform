537efd1820d88a9afe7c5854674fa1389034411f
SHtr3s <stas.butrin1331@gmail.com>
2023-02-25 16:15:03 +0300

    fix: update sc-component-manager script

--- scripts/run_sc_component_manager.sh
+++ scripts/run_sc_component_manager.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 APP_ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
-$APP_ROOT_PATH/sc-machine/bin/sc-component-manager -c $APP_ROOT_PATH/ostis-web-platform.ini
+cd $APP_ROOT_PATH/sc-machine/bin
+./sc-component-manager -c $APP_ROOT_PATH/ostis-web-platform.ini
