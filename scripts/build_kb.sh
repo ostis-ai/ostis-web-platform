bfc3bee2b1969baaf3898a77c12ae61c1befa1c7
NikitaZotov <nikita.zotov.belarus@gmail.com>
2023-03-29 12:54:40 +0300

    docs: apply changes

--- scripts/build_kb.sh
+++ scripts/build_kb.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 ../sc-machine/scripts/build_kb.py ../repo.path -c ../ostis-web-platform.ini
+python3 ../sc-machine/scripts/build_kb.py ../repo.path -c ../ostis-web-platform.ini -b ../sc-machine/bin
+
