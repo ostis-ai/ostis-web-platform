02d8d3b660fdcd3ebdd60426ab8f98d2023c835c
SHtress <stas.butrin1331@gmail.com>
2023-04-06 21:59:22 +0300

    fix: add shebang

--- scripts/run_sc_component_manager.sh
+++ scripts/run_sc_component_manager.sh
@@ -1,4 +1,5 @@
-#/bin/bash
+#!/bin/bash
+
 APP_ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
-cd $APP_ROOT_PATH/sc-machine/bin
-./sc-component-manager -c $APP_ROOT_PATH/ostis-web-platform.ini
+
+$APP_ROOT_PATH/sc-machine/bin/sc-component-manager -c $APP_ROOT_PATH/ostis-web-platform.ini $1
