d25629978a2c2d68e686662f1301079046aa9bac
NikitaZotov <nikita.zotov.belarus@gmail.com>
2023-06-20 15:14:45 +0300

    ci: add set vars

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -3,16 +3,35 @@ set -eo pipefail
 
 APP_ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
 
-export APP_ROOT_PATH="${APP_ROOT_PATH}"
-export PLATFORM_PATH="${APP_ROOT_PATH}"
-export SCRIPTS_PATH="${PLATFORM_PATH}/scripts"
-export BINARY_PATH="${PLATFORM_PATH}/sc-machine/bin"
-export CONFIG_PATH="${PLATFORM_PATH}/ostis-web-platform.ini"
+if [ "$1" == "--ci" ];
+then
+  {
+    echo APP_ROOT_PATH="${APP_ROOT_PATH}"
+    echo PLATFORM_PATH="${APP_ROOT_PATH}"
+    echo SCRIPTS_PATH="${PLATFORM_PATH}/scripts"
+    echo BINARY_PATH="${PLATFORM_PATH}/sc-machine/bin"
+    echo CONFIG_PATH="${PLATFORM_PATH}/ostis-web-platform.ini"
 
-export SC_MACHINE_REPO="https://github.com/ostis-ai/sc-machine.git"
-export SC_MACHINE_BRANCH="main"
-export SC_MACHINE_PATH="${PLATFORM_PATH}/sc-machine"
+    echo SC_MACHINE_REPO="https://github.com/ostis-ai/sc-machine.git"
+    echo SC_MACHINE_BRANCH="main"
+    echo SC_MACHINE_PATH="${PLATFORM_PATH}/sc-machine"
 
-export SC_WEB_REPO="https://github.com/ostis-ai/sc-web.git"
-export SC_WEB_BRANCH="main"
-export SC_WEB_PATH="${PLATFORM_PATH}/sc-web"
+    echo SC_WEB_REPO="https://github.com/ostis-ai/sc-web.git"
+    echo SC_WEB_BRANCH="main"
+    echo SC_WEB_PATH="${PLATFORM_PATH}/sc-web"
+  } >> "$GITHUB_ENV"
+else
+  export APP_ROOT_PATH="${APP_ROOT_PATH}"
+  export PLATFORM_PATH="${APP_ROOT_PATH}"
+  export SCRIPTS_PATH="${PLATFORM_PATH}/scripts"
+  export BINARY_PATH="${PLATFORM_PATH}/sc-machine/bin"
+  export CONFIG_PATH="${PLATFORM_PATH}/ostis-web-platform.ini"
+
+  export SC_MACHINE_REPO="https://github.com/ostis-ai/sc-machine.git"
+  export SC_MACHINE_BRANCH="main"
+  export SC_MACHINE_PATH="${PLATFORM_PATH}/sc-machine"
+
+  export SC_WEB_REPO="https://github.com/ostis-ai/sc-web.git"
+  export SC_WEB_BRANCH="main"
+  export SC_WEB_PATH="${PLATFORM_PATH}/sc-web"
+fi
