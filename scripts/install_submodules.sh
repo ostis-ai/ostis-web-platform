becaaada54b1c319d819ea76be161f6cdef7eae4
NikitaZotov <nikita.zotov.belarus@gmail.com>
2023-07-28 12:50:26 +0300

    feat: add commit env for submodules install

--- scripts/install_submodules.sh
+++ scripts/install_submodules.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 CURRENT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
 source "${CURRENT_DIR}/formats.sh"
 
-if [[ -z "${PLATFORM_PATH}" || -z "${SC_MACHINE_REPO}" || -z "${SC_WEB_REPO}" \
-  || -z "${SC_MACHINE_BRANCH}" || -z "${SC_WEB_BRANCH}" ]];
+if [[ -z "${PLATFORM_PATH}" || -z "${SC_MACHINE_PATH}" || -z "${SC_WEB_PATH}" || -z "${SC_MACHINE_REPO}" || -z "${SC_WEB_REPO}" \
+  || -z "${SC_MACHINE_BRANCH}" || -z "${SC_WEB_BRANCH}" || -z "${SC_MACHINE_COMMIT}" || -z "${SC_WEB_COMMIT}" ]];
 then
   source "${CURRENT_DIR}/set_vars.sh"
 fi
@@ -32,7 +32,8 @@ clone_project()
     exit 1
   fi
 
-  if [[ ! -d "$2" || ${update} == 1 ]]; then
+  if [[ ! -d "$2" || ${update} == 1 ]];
+  then
     if (( ${update} == 1 ));
     then
       printf "Remove submodule %s (%s) %s \n" "$1" "$3" "$2"
@@ -40,8 +41,17 @@ clone_project()
       git pull
     fi
 
-    printf "Clone submodule %s (%s) into %s\n" "$1" "$3" "$2"
+    if [ -n "$4" ];
+    then
+      printf "Clone submodule %s (%s %s) into %s\n" "$1" "$3" "$4" "$2"
+    else
+      printf "Clone submodule %s (%s) into %s\n" "$1" "$3" "$2"
+    fi
     git clone "$1" --branch "$3" --single-branch "$2" --recursive
+    if [ -n "$4" ];
+    then
+      cd "$2" && git checkout "$4"
+    fi
   else
     printf "You can update %s manually. Use this script with \"update\" parameter.\n" "$2"
   fi
@@ -75,8 +85,8 @@ stage "Clone submodules"
 
 cd "${PLATFORM_PATH}"
 
-clone_project "${SC_MACHINE_REPO}" "${SC_MACHINE_PATH}" "${SC_MACHINE_BRANCH}"
-clone_project "${SC_WEB_REPO}" "${SC_WEB_PATH}" "${SC_WEB_BRANCH}"
+clone_project "${SC_MACHINE_REPO}" "${SC_MACHINE_PATH}" "${SC_MACHINE_BRANCH}" "${SC_MACHINE_COMMIT}"
+clone_project "${SC_WEB_REPO}" "${SC_WEB_PATH}" "${SC_WEB_BRANCH}" "${SC_WEB_COMMIT}"
 git submodule update --init --recursive
 
 stage "Submodules cloned successfully"
