feab1520557007f1e7e7680ab5fbd1f8aaf6d7a4
NikitaZotov <nikita.zotov.belarus@gmail.com>
2023-10-23 15:50:59 +0300

    feat: add wave_find_path_scp program

--- scripts/build_scp_machine.sh
+++ scripts/build_scp_machine.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -fexeo pipefail
 
 if [ -z "${SCP_MACHINE_PATH}" ];
 then
-  source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"/set_vars.sh
+  source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"/../scp-machine/scripts/set_vars.sh
 fi
 
-"${SCP_MACHINE_PATH}/scripts/build_scp_machine.sh" "$@"
+cd "${SCP_MACHINE_PATH}" && ./scripts/build_scp_machine.sh "$@"
