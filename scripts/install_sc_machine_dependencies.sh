c8ec7ef5a6428aadb03462771ff1de856554d2f7
MksmOrlov <orlovmaksimkonstit@gmail.com>
2023-11-27 21:08:18 +0300

    [scripts] Remove installation of component manager dependencies

--- scripts/install_sc_machine_dependencies.sh
+++ scripts/install_sc_machine_dependencies.sh
@@ -9,22 +9,8 @@ then
   source "${CURRENT_DIR}/set_vars.sh"
 fi
 
-args=()
-install_component_manager_dependencies=false
-for arg in "$@"; do
-  if [ "$arg" != "-cm" ]; then
-      args+=("$arg")
-  else install_component_manager_dependencies=true
-  fi
-done
-
 stage "Install sc-machine dependencies"
 
-"${SC_MACHINE_PATH}/scripts/install_deps_ubuntu.sh" "${args[@]}"
-
-if [ $install_component_manager_dependencies ]; 
-then
-  "${SC_MACHINE_PATH}/scripts/install_sc_component_manager_dependencies.sh" "${args[@]}"
-fi
+"${SC_MACHINE_PATH}/scripts/install_deps_ubuntu.sh" "$@"
 
 stage "Dependencies of sc-machine installed successfully"
