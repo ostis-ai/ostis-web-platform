85a818726419d072ab4b034e73a14e7bc6faa32b
Nikita Zotov <nikita.zotov.belarus@gmail.com>
2024-01-27 16:29:12 +0300

    scripts: set release 0.9.0 modules

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -10,7 +10,7 @@ export CONFIG_PATH="${CONFIG_PATH:-${PLATFORM_PATH}/ostis-web-platform.ini}"
 export REPO_PATH="${REPO_PATH:-${PLATFORM_PATH}/repo.path}"
 
 export SC_MACHINE_REPO="${SC_MACHINE_REPO:-https://github.com/ostis-ai/sc-machine.git}"
-export SC_MACHINE_BRANCH="${SC_MACHINE_BRANCH:-main}"
+export SC_MACHINE_BRANCH="${SC_MACHINE_BRANCH:-0.9.0-Unlock}"
 export SC_MACHINE_COMMIT="${SC_MACHINE_COMMIT:-""}"
 export SC_MACHINE_PATH="${SC_MACHINE_PATH:-${PLATFORM_PATH}/sc-machine}"
 
@@ -20,7 +20,7 @@ export SCP_MACHINE_COMMIT="${SCP_MACHINE_COMMIT:-""}"
 export SCP_MACHINE_PATH="${SCP_MACHINE_PATH:-${PLATFORM_PATH}/scp-machine}"
 
 export SC_WEB_REPO="${SC_WEB_REPO:-https://github.com/ostis-ai/sc-web.git}"
-export SC_WEB_BRANCH="${SC_WEB_BRANCH:-main}"
+export SC_WEB_BRANCH="${SC_WEB_BRANCH:-0.8.1-Unlock}"
 export SC_WEB_COMMIT="${SC_WEB_COMMIT:-""}"
 export SC_WEB_PATH="${SC_WEB_PATH:-${PLATFORM_PATH}/sc-web}"
 
