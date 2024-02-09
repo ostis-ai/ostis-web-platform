6d7ffb265abb75ee16f1d6e4652f82df28b6b7c8
Anna Makarenko <manahafor4@gmail.com>
2024-02-09 17:41:42 +0300

    scripts: don't reset envs from sc-machine; use unique envs for sc-component-manager

--- scripts/build_sc_machine.sh
+++ scripts/build_sc_machine.sh
@@ -6,6 +6,4 @@ then
   source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"/set_vars.sh
 fi
 
-export ROOT_CMAKE_PATH="${SC_MACHINE_PATH}"
-export BUILD_PATH="${SC_MACHINE_PATH}/build"
 "${SC_MACHINE_PATH}/scripts/build_sc_machine.sh" "$@"
