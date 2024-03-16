4d4515d450b756e335887f6adfb0aac39a16cfc0
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-03-16 20:08:41 +0300

    scripts: fix installation scripts

--- scripts/install_minimal_platform.sh
+++ scripts/install_minimal_platform.sh
@@ -9,8 +9,10 @@ stage "Install ostis-web-platform"
 
 "${SUBMODULE_SCRIPTS_DIR}/install_sc_machine.sh"
 "${SCRIPTS_DIR}/install_sc_machine_dependencies.sh" --dev
+"${SUBMODULE_SCRIPTS_DIR}/install_scp_machine.sh"
 "${SUBMODULE_SCRIPTS_DIR}/install_sc_component_manager.sh"
 "${SCRIPTS_DIR}/build_sc_machine.sh"
+"${SCRIPTS_DIR}/build_scp_machine.sh"
 "${SCRIPTS_DIR}/build_sc_component_manager.sh"
 "${SCRIPTS_DIR}/build_kb.sh"
 
--- scripts/install_platform.sh
+++ scripts/install_platform.sh
@@ -9,6 +9,8 @@ stage "Install ostis-web-platform"
 "${SCRIPTS_DIR}/install_submodules.sh"
 "${SCRIPTS_DIR}/install_dependencies.sh" --dev
 "${SCRIPTS_DIR}/build_sc_machine.sh"
+"${SCRIPTS_DIR}/build_scp_machine.sh"
+"${SCRIPTS_DIR}/build_sc_component_manager.sh"
 "${SCRIPTS_DIR}/build_sc_web.sh"
 "${SCRIPTS_DIR}/build_kb.sh"
 
