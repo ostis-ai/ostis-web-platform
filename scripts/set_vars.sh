8cac12fe98dc5e4dbce8f7ae1f4fd217e231f44e
NikitaZotov <nikita.zotov.belarus@gmail.com>
2025-05-19 21:09:25 +0300

    fix: pin ims.ostis-kb branch

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -29,7 +29,7 @@ export SC_WEB_COMMIT="${SC_WEB_COMMIT:-""}"
 export SC_WEB_PATH="${SC_WEB_PATH:-${PLATFORM_PATH}/interface/sc-web}"
 
 export IMS_KB_REPO="${IMS_KB_REPO:-https://github.com/ostis-ai/ims.ostis.kb.git}"
-export IMS_KB_BRANCH="${IMS_KB_BRANCH:-""}"
+export IMS_KB_BRANCH="${IMS_KB_BRANCH:-main}"
 export IMS_KB_COMMIT="${IMS_KB_COMMIT:-"0.3.0"}"
 export IMS_KB_PATH="${IMS_KB_PATH:-${PLATFORM_PATH}/kb/ims.ostis.kb}"
 
